9a267e90324d0427119f79e9ca644ab7c02df1d7
arichild <arieevq@gmail.com>
2022-04-30 02:39:04 +0300

    save

---
+++ src/js/slider.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+  loop: false,
+  
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+});
